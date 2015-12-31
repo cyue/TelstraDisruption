9a9ef8df777ec5a952e9f67a9d25482900e6e435
cyue <ansorc@163.com>
2015-12-31 05:48:46 +0000

    finish decision_tree.py

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -5,7 +5,7 @@ from sklearn.tree import DecisionTreeClassifier as dtc
 from sklearn import cross_validation as cv
 
 def get_training_data(file_train):
-    data = np.genfromtxt(fname='../train.csv', delimiter=',', usecols=(1,2,3,4,5,6,7))
+    data = np.genfromtxt(fname=file_train, delimiter=',')
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
 
@@ -14,10 +14,37 @@ def get_training_data(file_train):
     return x, y
 
 def get_test_data(file_test):
-    data = np.genfromtxt(fname='../test.csv', delimiter=',', usecols=(1,2,3,4,5,6))
+    data = np.genfromtxt(fname=file_test, delimiter=',') 
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
 
     return data
 
-def train(train_x, train_y, test_x
+def train(x,y):
+    classifier = dtc()
+    scores = cv.cross_val_score(classifier, x, y, cv=10)
+    
+    classifier.fit(x, y)
+    return classifier, scores
+
+def test(classifier, x):
+    preds = classifier.predict(x)
+    return x, preds
+
+
+def main():
+    x, y = get_training_data('../train.csv')
+    data = get_test_data('../test.csv')
+    c, scores = train(x,y)
+    x, preds = test(c,data)
+
+    print scores
+
+if __name__ == '__main__':
+    main()
+    
+
+    
+    
+
+
