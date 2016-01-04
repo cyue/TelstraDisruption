8f9df6e2c7bf2eeb4716efe00b334b60ec0f911c
cyue <ansorc@163.com>
2016-01-04 09:05:55 +0000

    rf v2.0

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -13,7 +13,7 @@ def get_training_data(file_train):
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
 
-    x = data[:,:-1]
+    x = data[:,1:-1]
     y = data[:,-1]
     return x, y
 
@@ -23,7 +23,7 @@ def get_test_data(file_test):
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
 
-    return data
+    return data[:,1:]
 
 
 def train(x,y,depth=10):
--- src/random_forest.py
+++ src/random_forest.py
@@ -21,12 +21,12 @@ def samples(file_train, size=5000):
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
 
-    x = data[:size,:-1]
+    x = data[:size,1:-1]
     y = data[:size, -1]
     return x, y
 
 
-def train(x, y, depth=10):
+def train(x, y, depth=None):
     rfc = RandomForestClassifier(max_depth=depth)
     rfc.fit(x,y)
     
@@ -46,29 +46,46 @@ def evaluation(x, y, test_x, test_y):
         rfc.fit(x,y)
 
         pred_y = rfc.predict(test_x)
-        confusion_matrixes[max_feature] = confusion_matrix(test_y, pred_y).reshape(9)
+        confusion_matrixes[max_feature] = confusion_matrix(test_y, pred_y).ravel()
 
-        confusion_df = pd.DataFrame(confusion_matrixes)
+    confusion_df = pd.DataFrame(confusion_matrixes)
 
+    f, ax = plt.subplots(figsize=(7, 5))
+    confusion_df.plot(kind='bar', ax=ax)
+    ax.legend(loc='best')
+    ax.set_title("Guessed vs Correct (i, j) where i is the guess and j is the actual.")
+    ax.grid()
+    ax.set_xticklabels([str((i, j)) for i, j in list(itertools.product(range(3), range(3)))]);
+    ax.set_xlabel("Guessed vs Correct")
+    ax.set_ylabel("Correct")
+    savefig('../img/random_forest_confusion_matrix.jpg')
+
+    n_estimator_params = range(1,50)
+    confusion_matrixes = {}
+    for n_est in n_estimator_params:
+        rfc = RandomForestClassifier(n_estimators=n_est)
+        rfc.fit(x,y)
+        pred_y = rfc.predict(test_x)
+        confusion_matrixes[n_est] = confusion_matrix(test_y, pred_y)
+
+        accuracy = lambda x: np.trace(x) / np.sum(x, dtype=float)
+        confusion_matrixes[n_est] = accuracy(confusion_matrixes[n_est])
+        accuracy_series = pd.Series(confusion_matrixes)
         f, ax = plt.subplots(figsize=(7, 5))
-        confusion_df.plot(kind='bar', ax=ax)
-        ax.legend(loc='best')
-        ax.set_title("Guessed vs Correct (i, j) where i is the guess and j is the actual.")
+        accuracy_series.plot(kind='bar', ax=ax, color='k', alpha=.75)
         ax.grid()
-        ax.set_xticklabels([str((i, j)) for i, j in
-                       list(itertools.product(range(2), range(2)))]);
-        ax.set_xlabel("Guessed vs Correct")
-        ax.set_ylabel("Correct")
-        savefig('../img/random_forest_confusion_matrix.jpg')
-        
-    
+        ax.set_title("Accuracy by Number of Estimators")
+        ax.set_ylim(0, 1) # we want the full scope
+        ax.set_ylabel("Accuracy")
+        ax.set_xlabel("Number of Estimators")
+    savefig('../img/rf_estimators.jpg')
 
 def get_test_data(file_test):
     data = np.genfromtxt(fname=file_test, delimiter=',') 
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
-
-    return data
+    
+    return data[:,1:]
 
 
 
@@ -78,11 +95,12 @@ def test(classifier, x):
 
 
 def main():
-    x, y = samples('../train.csv', size=5000)
+    x, y = samples('../train.csv', size=50000)
     test_x, test_y = samples('../train.csv', size=100)
     data = get_test_data('../test.csv')
 
-    evaluation(x,y,test_x,test_y)
+    #evaluation(x,y,test_x,test_y)
+    print train(x,y)[0]
 
 if __name__ == '__main__':
     main()
