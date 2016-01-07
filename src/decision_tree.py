fix test set scaling error

src/decision_tree.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier as dtc
 from sklearn import cross_validation as cv
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix, f1_score
 
 from StringIO import StringIO
 import pydot
@@ -30,15 +32,17 @@ def get_test_data(file_test):
     return data[:,1:], data[:,0]
 
 
-def train(x,y,depth=None):
+def train(x,y,depth=None, scale=True):
+    if scale:
+        x = preprocessing.scale(x)
+    
     classifier = dtc(max_depth=depth, class_weight='balanced')
-    scores = cv.cross_val_score(classifier, x, y, cv=10)
     
     classifier.fit(x, y)
     ne0 = classifier.feature_importances_ != 0
     y_imp = classifier.feature_importances_[ne0]
     x_imp = np.arange(len(classifier.feature_importances_))[ne0]
-    return classifier, scores, x_imp, y_imp
+    return classifier, x_imp, y_imp
 
 
 def test(classifier, x):
@@ -53,17 +57,37 @@ def decision_graph(classifier):
     graph.write("../img/decision_graph.jpg")
 
 
+def print_r(scale=True, size=10000):
+    x, y = get_training_data('../train.csv')
+    if scale:
+        x = preprocessing.scale(x)
+
+    classifier = dtc(class_weight='balanced')
+    score = cv.cross_val_score(classifier, x, y, cv=10, scoring='roc_auc')
+    
+    #classifier = dtc(class_weight='balanced')
+    #classifier.fit(x[:size],y[:size])
+    #preds = classifier.predict(x[size:])
+    
+    #f1 = f1_score(y[size:], preds, labels=[0.0,1.0,2.0], average='weighted')
+    
+
+    print score
+
+
 def main():
     x, y = get_training_data('../train.csv')
     data, ids = get_test_data('../test.csv')
-    c, scores, x_imp, y_imp = train(x,y)
+    c, x_imp, y_imp = train(x,y)
     preds = test(c, data)
     #decision_graph(c)
 
+    print 'id,predict_0,predict_1,predict_2'
     for tid, label in toolkit.vote(ids, preds):
         print '%s,%s' % (tid, ','.join([np.str(item) for item in label]))
 
 if __name__ == '__main__':
-    main()
+    #main()
+    print_r()
     
 

src/random_forest.py
@@ -10,27 +10,41 @@ from matplotlib.pyplot import savefig
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier 
 from sklearn import cross_validation as cv
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, f1_score
+from sklearn import preprocessing
 
+import toolkit
 
 from StringIO import StringIO
 import pydot
 
-def samples(file_train, size=5000):
+def get_train_data(file_train, scale=True, size=None):
     data = np.genfromtxt(fname=file_train, delimiter=',')
     data[:,6] = np.floor(data[:,6]/10)
     np.random.shuffle(data)
 
     x = data[:size,1:-1]
+    if scale:
+        x = preprocessing.scale(x)
     y = data[:size, -1]
     return x, y
 
 
-def train(x, y, depth=None):
-    rfc = RandomForestClassifier(max_depth=depth)
+def get_test_data(file_test, scale=True):
+    data = np.genfromtxt(fname=file_test, delimiter=',') 
+    data[:,6] = np.floor(data[:,6]/10)
     
-    scores = cv.cross_val_score(rfc, x, y, cv=6)
-    return scores, rfc
+    x, ids = data[:,1:], data[:,0]
+    if scale:
+        x = preprocessing.scale(x)
+
+    return x, ids 
+
+def train(x, y, depth=None):
+    rfc = RandomForestClassifier(n_estimators=50, max_depth=depth, class_weight='balanced', n_jobs=-1)
+    
+    rfc.fit(x,y)
+    return rfc
 
 
 def evaluation(x, y, test_x, test_y):
@@ -79,30 +93,44 @@ def evaluation(x, y, test_x, test_y):
         ax.set_xlabel("Number of Estimators")
     savefig('../img/rf_estimators.jpg')
 
-def get_test_data(file_test):
-    data = np.genfromtxt(fname=file_test, delimiter=',') 
-    data[:,6] = np.floor(data[:,6]/10)
-    np.random.shuffle(data)
-    
-    return data[:,1:]
-
 
 
 def test(classifier, x):
     preds = classifier.predict(x)
-    return x, preds
+    return preds
 
+
+def print_r(scale=True, size=10000):
+    x, y = get_train_data('../train.csv')
+    if scale:
+        x = preprocessing.scale(x)
+
+    classifier = RandomForestClassifier(n_estimators=20, class_weight='balanced', n_jobs=-1)
+    accuracy = cv.cross_val_score(classifier, x, y, cv=10)
+
+    classifier = RandomForestClassifier(n_estimators=20, class_weight='balanced', n_jobs=-1)
+    
+    classifier.fit(x[:size],y[:size])
+    preds = classifier.predict(x[size:])
+    
+    f1 = f1_score(y[size:], preds, labels=[0.0,1.0,2.0], average='weighted')
+
+    print '%s\n%s' % (f1, accuracy)
 
 def main():
-    x, y = samples('../train.csv', size=50000)
-    test_x, test_y = samples('../train.csv', size=100)
-    data = get_test_data('../test.csv')
-
+    x, y = get_train_data('../train.csv')
+    data, ids = get_test_data('../test.csv')
+    rfc = train(x, y)
     #evaluation(x,y,test_x,test_y)
-    print train(x,y)[0]
+
+    preds = test(rfc, data)
+    print 'id,predict_0,predict_1,predict_2'
+    for tid, label in toolkit.vote(ids, preds):
+        print '%s,%s' % (tid, ','.join([np.str(item) for item in label]))
 
 if __name__ == '__main__':
     main()
+    #print_r()
     
     
 

src/toolkit.py
@@ -17,5 +17,10 @@ def vote(ids, predicts):
     
     for id in id_dict:
         # return id, the index(label) which is the majority
-        yield id, [item / np.max(id_dict[id]) for item in id_dict[id]]
+        label_list = [item / np.max(id_dict[id]) for item in id_dict[id]]
+        if np.sum(label_list) > 1:
+            # randomly set one to 0
+            label_list[label_list.index(1)] = 0
+            
+        yield id, label_list
         